37addd3babfe2819797924395e54eed2a44f87dc
ZHU Xinyu <xzhuah@connect.ust.hk>
2019-10-06 01:40:14 -0700

    update API

--- library/src/utils/docker_manager.py
+++ library/src/utils/docker_manager.py
@@ -7,12 +7,12 @@ from utils.annotations import simple_thread
 
 
 @simple_thread
-def build_image(client, path, tag):
+def build_image(path, tag):
     client = docker.from_env()
     client.images.build(path=path, tag=tag)
 
 
-def get_ip_address(client, container_id):
+def get_ip_address(container_id):
     return docker.APIClient().inspect_container(container_id)['NetworkSettings']['Networks']['bridge']['IPAddress']
 
 
@@ -25,16 +25,16 @@ def query_container_id(client):
 
 
 def init_docker_environment(path, tag="htm/htm:1.0", timeout=600):
-    build_image()
+    build_image(path, tag)
 
     client = docker.from_env()
-    while len(query_container_id()) == 0:
+    while len(query_container_id(client)) == 0:
         time.sleep(1)
         timeout -= 1
         if timeout < 0:
             raise RuntimeError("Docker launch failed!")
 
-    ip_address = get_ip_address(client, query_container_id()[0])
+    ip_address = get_ip_address(query_container_id(client)[0])
     return ip_address
 
 
