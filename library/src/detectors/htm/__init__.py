8ebd28d08dca8876b19d129d6c42dbce7718ec69
ZHU Xinyu <xzhuah@connect.ust.hk>
2019-10-06 00:51:50 -0700

    testing docker server accessibility

--- library/src/detectors/htm/__init__.py
+++ library/src/detectors/htm/__init__.py
@@ -1,1 +1,3 @@
 # Created by Xinyu Zhu on 10/5/2019, 11:03 PM
+import sys
+sys.path.append("../../")
--- library/src/detectors/htm/htm_detector_docker_test.py
+++ library/src/detectors/htm/htm_detector_docker_test.py
@@ -1,13 +1,10 @@
 # Created by Xinyu Zhu on 10/5/2019, 11:02 PM
 
-import sys
-sys.path.append("../../")
 
 from common.http_api_client import HttpApiClient
 from utils.docker_manager_cmd import get_target_ip_address
 
 ip = get_target_ip_address("../../../docker/htmDocker/")
-
 
 print(ip)
 
