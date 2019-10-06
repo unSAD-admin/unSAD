f7f01b973118ad9bdd3641179b53b3dfba9aa7e3
ZHU Xinyu <xzhuah@connect.ust.hk>
2019-10-06 02:08:25 -0700

    optimize import

--- library/src/common/htm_docker_api.py
+++ library/src/common/htm_docker_api.py
@@ -1,6 +1,9 @@
 # Created by Xinyu Zhu on 10/6/2019, 1:42 AM
 
 from http_api_client import HttpApiClient
+import sys
+
+sys.path.append("../")
 from utils.docker_manager import init_docker_environment
 from utils.docker_manager_cmd import get_target_ip_address
 
