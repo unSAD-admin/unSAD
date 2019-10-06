595bec8d23eeca28eaec08f79a1b39762ad582b9
Ubuntu <ubuntu@ip-172-31-13-205.us-west-1.compute.internal>
2019-10-06 07:49:57 +0000

    test

--- library/src/detectors/htm/htm_detector_docker_test.py
+++ library/src/detectors/htm/htm_detector_docker_test.py
@@ -1,4 +1,8 @@
 # Created by Xinyu Zhu on 10/5/2019, 11:02 PM
+
+import sys
+sys.path.append("../../")
+
 from common.http_api_client import HttpApiClient
 from utils.docker_manager_cmd import get_target_ip_address
 
