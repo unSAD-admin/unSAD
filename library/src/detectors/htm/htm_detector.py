cdace6f6b529fa3882f4a27fb430ed54cbeaa153
shahaniyash <shahani.yash@gmail.com>
2019-10-06 15:32:18 -0700

    test 1

--- library/src/detectors/htm/htm_detector.py
+++ library/src/detectors/htm/htm_detector.py
@@ -2,8 +2,10 @@ import sys
 
 sys.path.append("../../")
 
+import time
 from detectors.base import BaseDetector
 from collections import defaultdict
+from common.htm_docker_api import HTMApiProvider
 
 from utils.collection_tools import normalize
 
@@ -13,8 +15,8 @@ class SequentialPatternAnomalyDetector(BaseDetector):
     def __init__(self, measure_col_names=None):
         # super(SequentialPatternAnomalyDetector, self).__init__(timestamp_col_name=None,
         #                                                        measure_col_names=measure_col_names, symbolic=True)
-        super(SequentialPatternAnomalyDetector, self).__init__(timestamp_col_name="Timestamp",
-                                                               measure_col_names="List", symbolic=False)
+        super(SequentialPatternAnomalyDetector, self).__init__(timestamp_col_name="timestamp",
+                                                               measure_col_names=["value"], symbolic=False)
     #     overwrite, pass diff params to default: "Timestamp","list",false
 
     def initialize(self, window_size=60, reduce_factor=2, *args, **kwargs):
@@ -71,8 +73,9 @@ class SequentialPatternAnomalyDetector(BaseDetector):
         record = self._pre_process_record(record)
         if record is None:
             raise RuntimeError("Data input does not match the input format")
-        # record will contain a list of values [timestamp, value] after pre_processing
-        #  PASS RECORD TO HTMApiProvider (htm_docker_api class) object, and return result to user
+        # TODO: record will contain a list of values [timestamp, value] after pre_processing
+        # TODO: PASS RECORD TO HTMApiProvider (htm_docker_api class) object, and return result to user
+
         # next_candidate = self._predict()
         # result = 0
         # if record in next_candidate:
@@ -86,3 +89,40 @@ class SequentialPatternAnomalyDetector(BaseDetector):
     def train(self, training_data):
         for record in training_data:
             self.handle_record(record)
+
+if __name__ == '__main__':
+    htm = HTMApiProvider(docker_path="../../docker/htmDocker/")
+    # Test basic API
+    print(htm.recycle_detector())
+    print(htm.set_max_detector_num(10))
+
+    # create new detector with default parameters
+    detector_key = htm.create_new_detector() # keep the detector_key
+
+
+    sample_list = [[123,5],[342,6]] # inner list is [timestamp,value]
+
+    print(detector_key)
+    result = []
+    now = time.time()
+    for list_element in sample_list:
+        # pass the data record to the detector
+        result.append(htm.pass_record_to_detector(detector_key, list_element[0], list_element[1])) # (key,timestamp,value)
+    t = time.time() - now
+    print(result)
+    print(t)
+
+    ts = []
+    vs = []
+    for list_element in sample_list:
+        ts.append(list_element[0])
+        vs.append(list_element[1])
+    now = time.time()
+    # pass an array of data to the detector
+    result = htm.pass_block_record_to_detector(detector_key, ts, vs)
+    t = time.time() - now
+
+    for r in result:
+        print(r)
+    print(result)
+    print(t)
