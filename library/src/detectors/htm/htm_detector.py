d617ac69e86be60c724e41cce14e4b0b5dfec224
ZHU Xinyu <xzhuah@connect.ust.hk>
2019-10-07 00:58:27 -0700

    Add some detectors in NAB repo

--- library/src/detectors/htm/htm_detector.py
+++ library/src/detectors/htm/htm_detector.py
@@ -10,7 +10,7 @@ class HTMAnomalyDetector(BaseDetector):
 
     def __init__(self, timestamp_col_name=None, value_col_name=None):
         if timestamp_col_name is None:  # since timestamp column name is essential for super class
-            timestamp_col_name = "Timestamp"
+            timestamp_col_name = "timestamp"
 
         super(HTMAnomalyDetector, self).__init__(timestamp_col_name=timestamp_col_name,
                                                  measure_col_names=[value_col_name], symbolic=False)
