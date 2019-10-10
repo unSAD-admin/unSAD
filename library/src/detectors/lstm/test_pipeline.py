3561bd986205aec0de542c40a3147b9862bfd1e7
han-qiu <hqiu4321@163.com>
2019-10-09 18:54:29 -0700

    Add seed for reproducibility; save validation result for parameter tuning; convert label to int;

--- library/src/detectors/lstm/test_pipeline.py
+++ library/src/detectors/lstm/test_pipeline.py
@@ -12,12 +12,13 @@ from utils.data_processor import Normalizer
 from common.dataset import SynthDataset, CSVDataset
 from detectors.lstm.lstm_detector import LSTMPredAnomalyDetector
 
-
 parser = argparse.ArgumentParser(
         description='Train and Test anomaly detection algorithm')
+parser.add_argument('--seed', type=int, default=1234,
+        help = 'seed for numpy and torch to maintain reproducibility')
 parser.add_argument('--dataset', type=str, default='synth',
         choices=['synth','yahoo'], help='type of dataset to use')
-parser.add_argument('--filepath', type=str, default='',
+parser.add_argument('--file_prefix', type=str, default='',
         help='filepath for the data file')
 parser.add_argument('--model', type=str, default='lstm-seq2seq')
 parser.add_argument('--epoches', type=int, default=200)
@@ -34,12 +35,18 @@ def anomaly_score(a, b):
     return np.abs(a-b)
 
 def main():
+    # reproducibility
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+
     conf_mat_list = []
+    score_list = []
+    y_val_list = []
     if args.dataset == "synth":
         # This is a hack, only one data file
         file_list = [""]
     elif args.dataset == 'yahoo':
-        file_list = glob.glob(args.filepath)
+        file_list = glob.glob(args.file_prefix+"*")
     else:
         raise ValueError("dataset %s not recognized" % args.dataset)
     for filename in file_list:
@@ -52,6 +59,8 @@ def main():
             data_train, data_test = dataset.getData()
             x_train, y_train = data_train["values"], data_train["label"]
             x_test, y_test = data_test["values"], data_test["label"]
+            y_train = y_train.astype(int)
+            y_test = y_test.astype(int)
         else:
             raise ValueError("dataset %s not recognized" % args.dataset)
         normalizer = Normalizer(zero_mean=True)
@@ -90,13 +99,20 @@ def main():
         if args.validate:
             score = anomaly_score(
                 x_pred_norm[len(x_train_torch):len(x_train_torch)+len(x_val_torch)], x_val_norm)
+            conf_mat = confusion_matrix((score > args.thresh).astype(int),
+                    y_val, labels=[0,1]).ravel()
+            conf_mat_list.append(conf_mat)
+            score_list.append(score)
+            y_val_list.append(y_val)
         else:
             score = anomaly_score(x_pred_norm[-len(x_test_torch):], x_test_norm)
-        if args.validate:
-            conf_mat = confusion_matrix((score > args.thresh).astype(int),
-                    y_val.astype('int'), labels=[0,1]).ravel()
-            conf_mat_list.append(conf_mat)
-        else:
+            # if there is label
+            try:
+                conf_mat = confusion_matrix((score > args.thresh).astype(int),
+                        y_test, labels=[0,1]).ravel()
+                conf_mat_list.append(conf_mat)
+            except NameError:
+                pass
             # de-normalize
             x_pred = normalizer.recoverData(x_pred_norm)
             # visualization
@@ -106,6 +122,12 @@ def main():
         rate = sum([a[i] for a in conf_mat_list])
         rate_list.append(rate)
     print("confusion matrix(tn, fp, fn, tp): ", rate_list)
+    if args.validate:
+        total_score = np.concatenate(tuple(score_list), 0)
+        total_y_val = np.concatenate(tuple(y_val_list), 0)
+        np.save("total_score", total_score)
+        np.save("total_y_val", total_y_val)
+
     # Do something with confusion matrix
 if __name__ == "__main__":
     main()
