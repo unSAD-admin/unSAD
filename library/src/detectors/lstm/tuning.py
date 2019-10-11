156502a6be491f5594b699055cc00f877a9205c6
han-qiu <hqiu4321@163.com>
2019-10-11 00:15:28 -0700

    add f1 for test data

--- library/src/detectors/lstm/tuning.py
+++ library/src/detectors/lstm/tuning.py
@@ -12,15 +12,18 @@ total_test_score = np.load(args.save_dir + "/total_test_score.npy")
 total_y_val = np.load(args.save_dir + "/total_y_val.npy")
 total_y_test = np.load(args.save_dir + "/total_y_test.npy")
 
-f1_list = []
-thresh_list = [a/100. for a in list(range(0, 1000, 10))]
+f1_val_list = []
+f1_test_list = []
+thresh_list = [a/10. for a in list(range(0, 20, 1))]
 for thresh in thresh_list:
     tn, fp, fn, tp = confusion_matrix(total_y_val,
             (total_val_score < thresh).astype(int), labels=[0,1]).ravel()
-    f1 = f1_score((total_val_score < thresh).astype(int), total_y_val, pos_label=1)
-    f1_list.append(f1)
+    f1_val = f1_score((total_val_score < thresh).astype(int), total_y_val, pos_label=1)
+    f1_test = f1_score((total_test_score < thresh).astype(int), total_y_test, pos_label=1)
+    f1_val_list.append(f1_val)
+    f1_test_list.append(f1_test)
 
 # import pdb;pdb.set_trace()
 import matplotlib.pyplot as plt
-plt.plot(thresh_list, f1_list)
+plt.plot(thresh_list, f1_val_list)
 plt.show()
