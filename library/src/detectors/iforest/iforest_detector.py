a701be4689f43831ae20cc306865cfb66c4c95a2
StrangeCloud9 <326902993@qq.com>
2019-11-17 00:26:48 -0800

    add init for iforest

---
+++ library/src/detectors/iforest/iforest_detector.py
@@ -0,0 +1,57 @@
+import sys
+import os 
+project_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(project_path)
+
+from detectors.base import BaseDetector
+from sklearn.ensemble import IsolationForest
+
+class IforestAnomalyDetecor(BaseDetector):
+
+    def __init__(self, timestamp_col_name=None, value_col_name=None):
+        if timestamp_col_name is None:
+            timestamp_col_name = 'timestamp'
+
+        super(IforestAnomalyDetecor,
+              self).__init__(timestamp_col_name=timestamp_col_name,
+                             measure_col_names=[value_col_name],
+                             symbolic=False)
+
+    def initialize(
+            self,
+            n_estimators=100,
+            max_samples="auto",
+            contamination=0.1,
+            max_features=1.,
+            bootstrap=False,
+            n_jobs=1,
+            behaviour='old',
+            random_state=None,
+            verbose=0,
+            *args, 
+            **kwargs
+            ):
+
+        super(IforestAnomalyDetecor, self).initialize(*args, **kwargs)
+        self.n_estimators = n_estimators
+        self.max_samples = max_samples
+        self.contamination = contamination
+        self.max_features = max_features 
+        self.bootstrap = bootstrap
+        self.n_jobs = n_jobs
+        self.behaviour = behaviour 
+        self.random_state = random_state
+        self.verbose = verbose 
+        # Create iforest instance
+
+        self.iforest = IsolationForest(n_estimators=self.n_estimators,
+                                             max_samples=self.max_samples,
+                                             contamination=self.contamination,
+                                             max_features=self.max_features,
+                                             bootstrap=self.bootstrap,
+                                             n_jobs=self.n_jobs,
+                                             behaviour=self.behaviour,
+                                             random_state=self.random_state,
+                                             verbose=self.verbose)
+
+
