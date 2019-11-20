772f4b32d99e79339b965711043bdbf17effd59b
xingyang-liu <billyliu@sjtu.edu.cn>
2019-11-20 14:50:31 -0800

    Add unittest

--- library/src/detectors/autoencoder/model.py
+++ library/src/detectors/autoencoder/model.py
@@ -10,13 +10,13 @@ class AutoEncoder(nn.Module):  # 16 8
         super(AutoEncoder, self).__init__()
         self.encode = nn.Sequential(
             nn.Linear(num_attributes, 16),
-            nn.Tanh(True),
+            nn.Tanh(),
             nn.Linear(16, 8),
             nn.ReLU(True)
         )
         self.decode = nn.Sequential(
             nn.Linear(8, 16),
-            nn.Tanh(True),
+            nn.Tanh(),
             nn.Linear(16, num_attributes),
             nn.ReLU(True)
         )
@@ -25,3 +25,18 @@ class AutoEncoder(nn.Module):  # 16 8
         x = self.encode(x)
         x = self.decode(x)
         return x
+
+
+class TestAutoEncoder(unittest.TestCase):
+    def test_dims(self):
+        batch_size = 10
+        for num_attributes in  [25, 35, 45]:
+            auto_encoder = AutoEncoder(num_attributes)
+            x = np.ones([batch_size, num_attributes], dtype=np.float32)
+            x = torch.FloatTensor(torch.from_numpy(x))
+            y = auto_encoder(x)
+            self.assertEqual(y.shape, torch.Size([batch_size, num_attributes]))
+
+
+if __name__ == "__main__":
+    unittest.main()
