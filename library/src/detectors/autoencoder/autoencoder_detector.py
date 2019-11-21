12cae773d6008067aba5571f45c3d0ba10a4fbe3
xingyang-liu <billyliu@sjtu.edu.cn>
2019-11-20 20:00:34 -0800

    Add visualization

--- library/src/detectors/autoencoder/autoencoder_detector.py
+++ library/src/detectors/autoencoder/autoencoder_detector.py
@@ -3,6 +3,10 @@ import random
 import torch
 import torch.optim as optim
 import os
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import unittest
 
 project_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(project_path)
@@ -30,7 +34,7 @@ class AutoEncoderDetector(BaseDetector):
         if self.use_gpu:
             self.model.cuda()
 
-    # train the data for num_epoches epoches
+    # train the data for num_epoches epochs
     @BaseDetector.require_initialize
     def train(self, x_train, num_epoches=300, verbose=False, learning_rate=1e-3):
         if self.use_gpu:
@@ -66,3 +70,38 @@ class AutoEncoderDetector(BaseDetector):
 
         return x_pred
 
+    def visualize(self, x_new, x_pred, y_label):
+        colors = ['dodgerblue', 'coral']
+        markers = ['o', '^']
+        # calculate MSE, MAE
+        mse = np.mean(np.power(x_new - x_pred, 2), axis=1)
+        mae = np.mean(np.abs(x_new - x_pred), axis=1)
+        index = np.arange(len(mse))
+        # plot
+        plt.figure(figsize=(14, 5))
+        plt.subplot(121)
+        plt.scatter(index, mse, c=y_label, cmap=matplotlib.colors.ListedColormap(colors))
+        plt.title('MSE for normal and abnormal data')
+        plt.xlabel('Index')
+        plt.ylabel('MSE')
+
+        plt.subplot(122)
+        plt.scatter(index, mae, c=y_label, cmap=matplotlib.colors.ListedColormap(colors))
+        plt.title('MAE for normal and abnormal data')
+        plt.xlabel('Index')
+        plt.ylabel('MAE')
+        plt.show()
+
+
+class TestAutoEncoderDetector(unittest.TestCase):
+    def test_vis(self):
+        x_new = np.array([1,2,3,3,2,6,2,3,7,1,2])
+        x_new = x_new.reshape((len(x_new), 1))
+        x_pred = np.array([2,2,3,4,2,3,2,2,3,1,2])
+        x_pred = x_pred.reshape((len(x_pred), 1))
+        y_label = np.array([0,0,0,0,0,1,0,0,1,0,0])
+        detector = AutoEncoderDetector()
+        detector.visualize(x_new, x_pred, y_label)
+
+if __name__ == '__main__':
+    unittest.main()
